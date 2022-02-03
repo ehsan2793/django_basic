858e3f7be7d0e126da6c2c56985cb4b35b273a4a
ehsan2793 <ehsan2793@gmail.com>
2022-02-03 01:54:45 -0800

    notes table added

--- djorg/djorg/settings.py
+++ djorg/djorg/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru9^nph!k87k&d1ds*q+#bfd)jb%(3$0($e7)qf6(!wt&c%32*'
+SECRET_KEY = 'django-insecure-$nile*hnxi4_y=f=g728db^*2xlv+^^e6g_x1vi%(py3m5g5xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
